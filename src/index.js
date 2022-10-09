b4468c8764edc9cf926e94d63f83ac8504ff93ba
nemo <appsbynemo@gmail.com>
2022-10-09 22:07:37 +0200

    downgraded to react 17.0.2 for blueprintjs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { BrowserRouter as Router } from "react-router-dom";
+// import reportWebVitals from './reportWebVitals';
+
+// import { BrowserRouter as Router } from "react-router-dom";
 import 'antd/dist/antd.css';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>,
-  </React.StrictMode>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
